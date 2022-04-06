719f4949faff3a88914ed4dcd3b03c8279c7d681
du99cy <du99cy@gmail.com>
2022-04-06 09:53:45 +0700

    'update'

--- app/routers/test/router.py
+++ app/routers/test/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile,Depends
+from auth.dependencies import get_current_active_user
 
 api_router = APIRouter(tags=["Test"],prefix="/test")
 
@@ -8,3 +9,8 @@ async def create_upload_file(
 ):
     
     return {"file":file}
+
+@api_router.get("/test_auth")
+async def test_auth(current_user=Depends(get_current_active_user)):
+    return current_user.dict()
+    
